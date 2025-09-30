40e4ac07c616f33fc63661a7cb74a894c49b3e60
munirajgowda <munirajgowdaraj@gmail.com>
2025-09-30 22:44:46 +0530

    login route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`);
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,15 +1,13 @@
 import express from "express";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
 };
 
-// Register route
 router.post("/register", async (req, res) => {
   try {
     const { email, username, password } = req.body;
@@ -19,17 +17,14 @@ router.post("/register", async (req, res) => {
     }
 
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password should be at least 6 characters long" });
+      return res.status(400).json({ message: "Password should be at least 6 characters long" });
     }
 
     if (username.length < 3) {
-      return res
-        .status(400)
-        .json({ message: "Username should be at least 3 characters long" });
+      return res.status(400).json({ message: "Username should be at least 3 characters long" });
     }
 
+    // check if user already exists
     const existingEmail = await User.findOne({ email });
     if (existingEmail) {
       return res.status(400).json({ message: "Email already exists" });
@@ -40,16 +35,13 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "Username already exists" });
     }
 
-    // Get random avatar
+    // get random avatar
     const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
-
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       email,
       username,
-      password: hashedPassword,
+      password,
       profileImage,
     });
 
@@ -60,21 +52,49 @@ router.post("/register", async (req, res) => {
     res.status(201).json({
       token,
       user: {
-        _id: user._id,
+        id: user._id,
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt,
       },
     });
   } catch (error) {
-    console.error("Error in register route", error);
+    console.log("Error in register route", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-// Login route
 router.post("/login", async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.status(400).json({ message: "All fields are required" });
+
+    // check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // check if password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+        createdAt: user.createdAt,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 export default router;
